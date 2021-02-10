9e2dc7794eda4fa364efd270ea3bb34fe978ee68
Denis Naughton <mjolinor75@hotmail.com>
2021-02-09 22:10:21 -0500

    Added info.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get 'static_pages/home'
+
+  get 'static_pages/info'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
