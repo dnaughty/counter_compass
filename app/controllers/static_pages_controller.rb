ed52e17de31681b697737c3bf6777452ffe17b1a
Denis Naughton <mjolinor75@hotmail.com>
2021-02-09 22:40:50 -0500

    Added misbins.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def info
   end
+
+  def misbins
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/info'
+   get 'static_pages/misbins'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
